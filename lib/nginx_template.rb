a827d237e60c52b1f8f22a1f576805adc08ef681
Mrls94 <sebasmrls94@gmail.com>
2018-07-26 11:47:45 -0500

    Changes to nginx conf file

--- lib/nginx_template.rb
+++ lib/nginx_template.rb
@@ -11,13 +11,12 @@ class NginxTemplate
   def template
     ERB.new("
               server {
-                listen 443;
+                server_name #{@domain};
+                root /home/ubuntu/coverapps;
+                listen 443 ssl;
                 ssl on;
                 ssl_certificate /home/ubuntu/ssl/#{domain}/#{@domain}.pem;
                 ssl_certificate_key /home/ubuntu/ssl/#{domain}/#{@domain}.key;
-                server_name #{@domain};
-                root ~/coverapps;
-                index index.html
                 access_log off;
                 client_max_body_size 10M;
                 keepalive_timeout 10;
